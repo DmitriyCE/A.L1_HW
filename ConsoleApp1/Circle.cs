a2a87ea49835210edd0ffc7095e3ec6719c97048
d.endeser1@gmail.com <d.endeser1@gmail.com>
2019-05-02 22:16:45 +0300

    Homework_completed

---
+++ ConsoleApp1/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A.L1_HW
+{
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public double AreaCirlce { get; set; }
+        public override void Area()
+        {
+            double area = Math.PI * Radius * Radius;
+            AreaCirlce = area;
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,44 @@ namespace A.L1_HW
     {
         static void Main(string[] args)
         {
+            var array = new double[10, 3];
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    switch (j)
+                    {
+                        case 0:
+                            {
+                                Circle circle = new Circle();
+                                circle.Radius = i;
+                                circle.Area();
+                                array[i, j] = circle.AreaCirlce;
+                                break;
+                            }
+                        case 1:
+                            {
+                                Square square = new Square();
+                                square.A = i;
+                                square.Area();
+                                array[i, j] = square.AreaSquare;
+                                break;
+                            }
+                        case 2:
+                            {
+                                Tringle tringle = new Tringle();
+                                tringle.BaseTriangle = i;
+                                tringle.HeightTriangle = i;
+                                tringle.Area();
+                                array[i, j] = tringle.AreaTriangle;
+                                break;
+                            }
+                    }
+                    Console.Write($"{array[i, j]}; ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
     }
 }
