802a57f2d7973758224c4538831c7d9a70c765e9
d.endeser1@gmail.com <d.endeser1@gmail.com>
2019-05-02 19:02:45 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A.L1_HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
